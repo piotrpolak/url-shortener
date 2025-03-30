9215a3007fd0019a720ca0dd412e284c9a08cb15
Piotr Polak <piotr@polak.ro>
2025-03-30 23:44:31 +0200

    SwaggerUI cleanup

--- app/src/main/java/ro/polak/urlshortener/config/AppWebMvcConfigurer.java
+++ app/src/main/java/ro/polak/urlshortener/config/AppWebMvcConfigurer.java
@@ -5,20 +5,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-class WebConfig implements WebMvcConfigurer {
+class AppWebMvcConfigurer implements WebMvcConfigurer {
 
-  private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-    "classpath:/META-INF/resources/", "classpath:/resources/",
-    "classpath:/static/", "classpath:/public/"
-  };
+  private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/"};
 
   /** Adds SwaggerUI support. */
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry
-        .addResourceHandler("/swagger-ui/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.36.1/");
-
     registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
   }
 }
---
+++ app/src/test/java/ro/polak/urlshortener/controller/StaticResourcesControllerITest.java
@@ -0,0 +1,20 @@
+package ro.polak.urlshortener.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ro.polak.urlshortener.support.DocumentationAndValidationMockMvcBuilderCustomizer.SKIP_OPEN_API_VALIDATION_ATTRIBUTE;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ro.polak.urlshortener.BaseIT;
+
+class StaticResourcesControllerITest extends BaseIT {
+
+  @ParameterizedTest
+  @CsvSource({"/swagger-ui.html", "/swagger-ui.css", "/url-shortener.yaml"})
+  public void should_create_shortcut_and_do_redirect(String uri) throws Exception {
+    mockMvc
+        .perform(get(uri).requestAttr(SKIP_OPEN_API_VALIDATION_ATTRIBUTE, true))
+        .andExpect(status().isOk());
+  }
+}
